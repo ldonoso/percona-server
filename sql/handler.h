290dcd7e15f1aa7c742c19973a1514880a63f70b
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-01-21 13:36:02 +0100

    FB8-89: RBR's idempotent mode to make slave crash safe (#1023)
    Summary:
    FB8-89: RBR's idempotent mode to make slave crash safe
    
    Jira issue: https://jira.percona.com/browse/FB8-89
    
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/6b65ec5
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/9278491
    
    8.0-specific changes.
    * Similarly to 'memroot_unordered_set' added 'memroot_unordered_map' class
      template to 'include/map_helpers.h'
    * 'binlog_recover()' now uses 'xid_to_gtid_container'
      ('memroot_unordered_map<my_xid, Gtid>') instead of plain-C 'HASH'.
    * In 'THD::set_trans_pos()' we now use 'owned_gtid' instead of third input
      parameter.
    * Partially cherry-picked 'worker_last_gtid'-related changes in 'Slave_worker'
      class from https://github.com/facebook/mysql-5.6/commit/b1b882b
    * Partially cherry-picked 'last_gtid'-related changes in 'Relay_log_info'
      class from https://github.com/facebook/mysql-5.6/commit/b1b882b
    * Removed 'mysql-test/suite/rpl_recovery/include/*' MTR include files from
      the original commit as they are identical to
      'mysql-test/suite/rpl/include/*' and are not used.
    * Removed 'mysql-test/suite/rpl_recovery/extension/*' MTR include files from
      the original commit as they are identical to
      'mysql-test/suite/rpl/extension/*' and are not used.
    * Removed 'innodb_use_native_aio = 0' from the
      'mysql-test/suite/rpl_recovery/rpl_1slave_base.cnf' and added
      'innodb_write_io_threads=2' / 'innodb_read_io_threads=0' instead as in 8.0.
    
    ---------- https://github.com/facebook/mysql-5.6/commit/6b65ec5 ----------
    
    RBR's idempotent mode to make slave crash safe
    Currently to ensure crash safety in slave a DB->GTID map is maintained
    which is updated after every commit. The map is required to identify holes
    in the sequence of gtids caused by using MTS. In RBR we can make use of
    idempotency of row events to handle holes instead. We only track the max gtid
    encountered and after a crash we turn on idempotency for all transactions which
    have gtid less than the max gtid. If any trx was already executed in this range
    it's okay because we've turned on idempotent mode.
    Closes https://github.com/facebook/mysql-5.6/issues/384
    
    Originally Reviewed By: santoshbanda
    
    Pulled By: yizhang82
    
    Differential Revision: D16812597
    
    -----------------------------------------------------------------------
    
    Enable idempotent recovery mode for TTL tables [non-RocksDB part]
    
    Summary:
    TTL is run independently on the master and slave, this causes data
    drift. Let's ignore idempotent errors on TTL enabled tables on the slave to work
    around this instead of using rbr_idempotent_tables.
    
    Reference Patch: https://github.com/facebook/mysql-5.6/commit/92a364f86100506426307b18239c9c825ae15c52
    
    Porting Notes:
    1. Re-enable corresponding tests
    2. Add warning suppression for HA_KEY_NOT_FOUND. The error reporting in MySQL 8.0 has change and it now prints warnings by default (the global variable warnings is deprecated).
    
    Reviewed By: lloyd
    
    Differential Revision: D17883249

--- sql/handler.h
+++ sql/handler.h
@@ -64,6 +64,7 @@
 #include "sql/dd/types/object_table.h"  // dd::Object_table
 #include "sql/discrete_interval.h"      // Discrete_interval
 #include "sql/key.h"
+#include "sql/rpl_gtid.h"        // GTID
 #include "sql/sql_const.h"       // SHOW_COMP_OPTION
 #include "sql/sql_list.h"        // SQL_I_List
 #include "sql/sql_plugin_ref.h"  // plugin_ref
@@ -1286,7 +1287,8 @@ typedef int (*rollback_t)(handlerton *hton, THD *thd, bool all);
 typedef int (*prepare_t)(handlerton *hton, THD *thd, bool all);
 
 typedef int (*recover_t)(handlerton *hton, XA_recover_txn *xid_list, uint len,
-                         MEM_ROOT *mem_root);
+                         MEM_ROOT *mem_root, Gtid *binlog_max_gtid,
+                         char *binlog_file, my_off_t *binlog_pos);
 
 /** X/Open XA distributed transaction status codes */
 enum xa_status_code {
@@ -6622,6 +6624,14 @@ class handler {
   int get_lock_type() const { return m_lock_type; }
 
   /**
+     Whether the storage engine table has a TTL column
+
+     @return true if the table contains ttl col
+             false otherwise
+   */
+  virtual bool has_ttl_column() const { return false; }
+
+  /**
     Callback function that will be called by my_prepare_gcolumn_template
     once the table has been opened.
   */
@@ -6953,6 +6963,7 @@ typedef bool Log_func(THD *, TABLE *, bool, const uchar *, const uchar *);
 
 int binlog_log_row(TABLE *table, const uchar *before_record,
                    const uchar *after_record, Log_func *log_func);
+int write_locked_table_maps(THD *thd);
 
 /* discovery */
 int ha_create_table_from_engine(THD *thd, const char *db, const char *name);
@@ -7001,7 +7012,11 @@ int ha_xa_prepare(THD *thd);
 */
 
 typedef ulonglong my_xid;  // this line is the same as in log_event.h
-int ha_recover(const mem_root_unordered_set<my_xid> *commit_list);
+using xid_to_gtid_container = mem_root_unordered_map<my_xid, Gtid>;
+
+int ha_recover(const xid_to_gtid_container *commit_list,
+               Gtid *binlog_max_gtid = nullptr, char *binlog_file = nullptr,
+               my_off_t *binlog_pos = nullptr);
 
 /**
   Perform SE-specific cleanup after recovery of transactions.
