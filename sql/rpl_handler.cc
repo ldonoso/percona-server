facebook/mysql-5.6

ability to stop and start threads from listener queue
Summary:
Replication threads need to be stopped and/or started at certain point when the
raft ring goes through reconfiguration (for ex: new leader election).
This diff brings in the ability for the same from 5.6. It is almost a direct
port of feature in 5.6 except for some changes around channel_map handling (due
to multi-source replication in 8.0)
1. start_slave(..) and stop_slave(..) functions need additional args based on
channel_map. These are grabbed correctly from the default channel
2. A dummy LEX_MASTER_INFO and LEX_SLAVE_CONNECTION objects are passed in
(similar to what rpl_channel_service_interface is doing in 8.0)

Reviewed By: anirbanr-fb

Differential Revision: D22460948

fbshipit-source-id: 796d2e1ad86

sql/rpl_handler.cc
@@ -70,6 +70,9 @@
 
 /** start of raft related extern funtion declarations  **/
 extern int rotate_binlog_file(THD *thd);
+extern int raft_stop_sql_thread(THD *thd);
+extern int raft_stop_io_thread(THD *thd);
+extern int raft_start_sql_thread(THD *thd);
 /** end of raft related extern funtion declarations  **/
 
 Trans_delegate *transaction_delegate;
@@ -1298,6 +1301,18 @@ extern "C" void *process_raft_queue(void *) {
       }
       case RaftListenerCallbackType::BINLOG_CHANGE_TO_BINLOG: {
         result.error= binlog_change_to_binlog();
+        break;
+      }
+      case RaftListenerCallbackType::STOP_SQL_THREAD: {
+        result.error= raft_stop_sql_thread(current_thd);
+        break;
+      }
+      case RaftListenerCallbackType::START_SQL_THREAD: {
+        result.error= raft_start_sql_thread(current_thd);
+        break;
+      }
+      case RaftListenerCallbackType::STOP_IO_THREAD: {
+        result.error= raft_stop_io_thread(current_thd);
         break;
       }
       case RaftListenerCallbackType::RAFT_LISTENER_THREADS_EXIT:

ldonoso/percona-server

sql/rpl_handler.cc
@@ -71,6 +71,9 @@
 
 /** start of raft related extern funtion declarations  **/
 extern int rotate_binlog_file(THD *thd);
+extern int raft_stop_sql_thread(THD *thd);
+extern int raft_stop_io_thread(THD *thd);
+extern int raft_start_sql_thread(THD *thd);
 /** end of raft related extern funtion declarations  **/
 
 Trans_delegate *transaction_delegate;
@@ -1638,6 +1641,18 @@ extern "C" void *process_raft_queue(void *) {
       }
       case RaftListenerCallbackType::BINLOG_CHANGE_TO_BINLOG: {
         result.error = binlog_change_to_binlog();
+        break;
+      }
+      case RaftListenerCallbackType::STOP_SQL_THREAD: {
+        result.error = raft_stop_sql_thread(current_thd);
+        break;
+      }
+      case RaftListenerCallbackType::START_SQL_THREAD: {
+        result.error = raft_start_sql_thread(current_thd);
+        break;
+      }
+      case RaftListenerCallbackType::STOP_IO_THREAD: {
+        result.error = raft_stop_io_thread(current_thd);
         break;
       }
       case RaftListenerCallbackType::RAFT_LISTENER_THREADS_EXIT: