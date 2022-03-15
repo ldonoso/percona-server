inikep/mysql-5.6

Make dual engine recovery work correctly [non-RocksDB part]
Summary:
In MyRocks8.0, we need both InnoDB and RocksDB engines running since
data dictionary and temp tables use InnoDB. Binlog positions are written
separately into each engine during trx commit time. At recovery, the
binlog position should be set to the minimum positions of all engines so
that when binlog is ahead of storage engine, the slave doesn't miss any
transactions from both storage engines. Also, max gtid is set to the max
across all engines in order for idempodent recovery to work.

Differential Revision: D18694795

sql/binlog.cc
@@ -5950,6 +5950,27 @@ uint split_file_name_and_gtid_set_length(char *file_name_and_gtid_set_length) {
   return 0;
 }
 
+bool is_binlog_advanced(const char *b1, my_off_t p1, const char *b2,
+                        my_off_t p2) {
+  if (!b1 || !b1[0]) {
+    return (b2 && b2[0] && p2 != ULLONG_MAX);
+  }
+
+  if (!b2 || !b2[0]) {
+    return false;
+  }
+
+  DBUG_ASSERT(p1 != ULLONG_MAX && p2 != ULLONG_MAX);
+
+  int cmp = strlen(b1) - strlen(b2);
+
+  if (cmp) {
+    return (cmp < 0);
+  }
+
+  return (strcmp(b1, b2) < 0 || (strcmp(b1, b2) == 0 && p1 < p2));
+}
+
 /**
   Find the position in the log-index-file for the given log name.
 

sql/xa.cc
@@ -308,6 +308,79 @@ bool Recovered_xa_transactions::recover_prepared_xa_transactions() {
   return ret;
 }
 
+/*
+ * Recover engine binlog position for plugins that have 2pc enabled.
+ * In order for multiple 2pc engines to recover properly, the binlog
+ * position should be the minimum of what each plugin has.
+ * Warning: if an engine has an empty binlog position, i.e., slave
+ * crashed earlier than the first binlog position update, that slave
+ * would lose those transactions and be inconsistent with master.
+ * Rather than replaying all binlogs from the very beginning, which
+ * may be infeasible, this slave needs to be reconstructed operationally.
+ * Periodically syncing binlog positions from these engines should reduce
+ * the chance of this problem.
+ */
+static void recover_binlog_pos(const char *plugin_name, handlerton *hton,
+                               xarecover_st *info) {
+  char binlog_file[FN_REFLEN + 1] = {0};
+  my_off_t binlog_pos = ULLONG_MAX;
+  Gtid max_gtid{0, 0};
+
+  DBUG_ASSERT(info->binlog_file && info->binlog_max_gtid);
+
+  hton->recover_binlog_pos(hton, &max_gtid, binlog_file, &binlog_pos);
+
+  if (binlog_file[0] == 0) {
+    DBUG_ASSERT(max_gtid.is_empty());
+
+    return;
+  }
+
+  char gtid_buf[Gtid::MAX_TEXT_LENGTH + 1] = {0};
+  if (!max_gtid.is_empty()) {
+    global_sid_lock->rdlock();
+    max_gtid.to_string(global_sid_map, gtid_buf);
+    global_sid_lock->unlock();
+  }
+
+  sql_print_information("Plugin '%s': binlog position (%s,%llu), max gtid %s",
+                        plugin_name, binlog_file, binlog_pos, gtid_buf);
+
+  if (info->binlog_file[0] == 0) {
+    DBUG_ASSERT(info->binlog_max_gtid->is_empty());
+
+    *(info->binlog_max_gtid) = max_gtid;
+    memcpy(info->binlog_file, binlog_file, FN_REFLEN + 1);
+    *info->binlog_pos = binlog_pos;
+
+    return;
+  }
+
+  // Compute the max gtid based on the max of that of each engine
+  // to make sure that idempotent recovery works
+  // Idempotent should be disabled if sidno (uuid) doesn't match
+  // in case of slave recovery after master switches. Also,
+  // different engines could have different sidno. This is
+  // not handled and slave would error out (duplicated key, key
+  // not found, etc) - however, it should be rare if we proactively
+  // keep all engines in sync.
+  if (max_gtid.greater_than(*info->binlog_max_gtid)) {
+    *(info->binlog_max_gtid) = max_gtid;
+
+    // binlog positions should monotonically increase with max gtid
+    DBUG_ASSERT(is_binlog_advanced(info->binlog_file, *info->binlog_pos,
+                                   binlog_file, binlog_pos));
+  }
+
+  // Compute mysql binlog position based on the min of that of
+  // each engine
+  if (is_binlog_advanced(binlog_file, binlog_pos, info->binlog_file,
+                         *info->binlog_pos)) {
+    memcpy(info->binlog_file, binlog_file, FN_REFLEN + 1);
+    *info->binlog_pos = binlog_pos;
+  }
+}
+
 static bool xarecover_handlerton(THD *, plugin_ref plugin, void *arg) {
   handlerton *hton = plugin_data<handlerton *>(plugin);
   xarecover_st *info = (struct xarecover_st *)arg;
@@ -325,9 +398,19 @@ static bool xarecover_handlerton(THD *, plugin_ref plugin, void *arg) {
   if (info->binlog_max_gtid) recovered_max_gtid = *(info->binlog_max_gtid);
 
   if (hton->state == SHOW_OPTION_YES && hton->recover) {
-    if (hton->recover_binlog_pos) {
-      hton->recover_binlog_pos(hton, info->binlog_max_gtid, info->binlog_file,
-                               info->binlog_pos);
+    if (info->binlog_file && hton->recover_binlog_pos) {
+      recover_binlog_pos(plugin_name(plugin)->str, hton, info);
+
+      char gtid_buf[Gtid::MAX_TEXT_LENGTH + 1] = {0};
+      if (info->binlog_max_gtid && !info->binlog_max_gtid->is_empty()) {
+        global_sid_lock->rdlock();
+        info->binlog_max_gtid->to_string(global_sid_map, gtid_buf);
+        global_sid_lock->unlock();
+      }
+
+      sql_print_information(
+          "Current chosen binlog position (%s,%llu), max gtid %s",
+          info->binlog_file, *info->binlog_pos, gtid_buf);
     }
 
     while (

ldonoso/percona-server

sql/binlog.cc
@@ -5983,6 +5983,27 @@ uint split_file_name_and_gtid_set_length(char *file_name_and_gtid_set_length) {
   return 0;
 }
 
+bool is_binlog_advanced(const char *b1, my_off_t p1, const char *b2,
+                        my_off_t p2) {
+  if (!b1 || !b1[0]) {
+    return (b2 && b2[0] && p2 != ULLONG_MAX);
+  }
+
+  if (!b2 || !b2[0]) {
+    return false;
+  }
+
+  assert(p1 != ULLONG_MAX && p2 != ULLONG_MAX);
+
+  int cmp = strlen(b1) - strlen(b2);
+
+  if (cmp) {
+    return (cmp < 0);
+  }
+
+  return (strcmp(b1, b2) < 0 || (strcmp(b1, b2) == 0 && p1 < p2));
+}
+
 /**
   Find the position in the log-index-file for the given log name.
 

sql/xa.cc
@@ -314,6 +314,79 @@ bool Recovered_xa_transactions::recover_prepared_xa_transactions() {
   return ret;
 }
 
+/*
+ * Recover engine binlog position for plugins that have 2pc enabled.
+ * In order for multiple 2pc engines to recover properly, the binlog
+ * position should be the minimum of what each plugin has.
+ * Warning: if an engine has an empty binlog position, i.e., slave
+ * crashed earlier than the first binlog position update, that slave
+ * would lose those transactions and be inconsistent with master.
+ * Rather than replaying all binlogs from the very beginning, which
+ * may be infeasible, this slave needs to be reconstructed operationally.
+ * Periodically syncing binlog positions from these engines should reduce
+ * the chance of this problem.
+ */
+static void recover_binlog_pos(const char *plugin_name, handlerton *hton,
+                               xarecover_st *info) {
+  char binlog_file[FN_REFLEN + 1] = {0};
+  my_off_t binlog_pos = ULLONG_MAX;
+  Gtid max_gtid{0, 0};
+
+  assert(info->binlog_file && info->binlog_max_gtid);
+
+  hton->recover_binlog_pos(hton, &max_gtid, binlog_file, &binlog_pos);
+
+  if (binlog_file[0] == 0) {
+    assert(max_gtid.is_empty());
+
+    return;
+  }
+
+  char gtid_buf[Gtid::MAX_TEXT_LENGTH + 1] = {0};
+  if (!max_gtid.is_empty()) {
+    global_sid_lock->rdlock();
+    max_gtid.to_string(global_sid_map, gtid_buf);
+    global_sid_lock->unlock();
+  }
+
+  sql_print_information("Plugin '%s': binlog position (%s,%llu), max gtid %s",
+                        plugin_name, binlog_file, binlog_pos, gtid_buf);
+
+  if (info->binlog_file[0] == 0) {
+    assert(info->binlog_max_gtid->is_empty());
+
+    *(info->binlog_max_gtid) = max_gtid;
+    memcpy(info->binlog_file, binlog_file, FN_REFLEN + 1);
+    *info->binlog_pos = binlog_pos;
+
+    return;
+  }
+
+  // Compute the max gtid based on the max of that of each engine
+  // to make sure that idempotent recovery works
+  // Idempotent should be disabled if sidno (uuid) doesn't match
+  // in case of slave recovery after master switches. Also,
+  // different engines could have different sidno. This is
+  // not handled and slave would error out (duplicated key, key
+  // not found, etc) - however, it should be rare if we proactively
+  // keep all engines in sync.
+  if (max_gtid.greater_than(*info->binlog_max_gtid)) {
+    *(info->binlog_max_gtid) = max_gtid;
+
+    // binlog positions should monotonically increase with max gtid
+    assert(is_binlog_advanced(info->binlog_file, *info->binlog_pos, binlog_file,
+                              binlog_pos));
+  }
+
+  // Compute mysql binlog position based on the min of that of
+  // each engine
+  if (is_binlog_advanced(binlog_file, binlog_pos, info->binlog_file,
+                         *info->binlog_pos)) {
+    memcpy(info->binlog_file, binlog_file, FN_REFLEN + 1);
+    *info->binlog_pos = binlog_pos;
+  }
+}
+
 static bool xarecover_handlerton(THD *, plugin_ref plugin, void *arg) {
   handlerton *hton = plugin_data<handlerton *>(plugin);
   xarecover_st *info = (struct xarecover_st *)arg;
@@ -331,9 +404,19 @@ static bool xarecover_handlerton(THD *, plugin_ref plugin, void *arg) {
   if (info->binlog_max_gtid) recovered_max_gtid = *(info->binlog_max_gtid);
 
   if (hton->state == SHOW_OPTION_YES && hton->recover) {
-    if (hton->recover_binlog_pos) {
-      hton->recover_binlog_pos(hton, info->binlog_max_gtid, info->binlog_file,
-                               info->binlog_pos);
+    if (info->binlog_file && hton->recover_binlog_pos) {
+      recover_binlog_pos(plugin_name(plugin)->str, hton, info);
+
+      char gtid_buf[Gtid::MAX_TEXT_LENGTH + 1] = {0};
+      if (info->binlog_max_gtid && !info->binlog_max_gtid->is_empty()) {
+        global_sid_lock->rdlock();
+        info->binlog_max_gtid->to_string(global_sid_map, gtid_buf);
+        global_sid_lock->unlock();
+      }
+
+      sql_print_information(
+          "Current chosen binlog position (%s,%llu), max gtid %s",
+          info->binlog_file, *info->binlog_pos, gtid_buf);
     }
 
     while (