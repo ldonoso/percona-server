facebook/mysql-5.6

Port Check before image consistency before applying row events
Summary:
New variable slave_check_before_image_consistency to check the full
before row image (when available) with the database state before applying the
event. This was already done when slave performs a table-scan and hash-scan so
we needed to add the check only to index-scan

Reference Patch: https://github.com/facebook/mysql-5.6/commit/d8c69f9f5a9a19d6977468d5ff45469e1c023cc7

Reviewed By: yizhang82

Differential Revision: D18460968

fbshipit-source-id: beed24c1bcd

sql/mysqld.cc
@@ -1169,6 +1169,7 @@ ulong binlog_checksum_options;
 ulong binlog_row_metadata;
 bool opt_master_verify_checksum = 0;
 bool opt_slave_sql_verify_checksum = 1;
+ulong opt_slave_check_before_image_consistency = 0;
 const char *binlog_format_names[] = {"MIXED", "STATEMENT", "ROW", NullS};
 bool binlog_gtid_simple_recovery;
 ulong binlog_error_action;
@@ -8478,6 +8479,27 @@ static int show_slave_running(THD *, SHOW_VAR *var, char *buff) {
   return 0;
 }
 
+static int show_slave_before_image_inconsistencies(THD *, SHOW_VAR *var,
+                                                   char *buff)
+{
+  channel_map.rdlock();
+  Master_info *mi = channel_map.get_default_channel_mi();
+
+  if (mi && mi->rli &&
+      mi->rli->check_before_image_consistency)
+  {
+    var->type= SHOW_LONGLONG;
+    var->value= buff;
+    *((ulonglong *)buff)=
+      (ulonglong) mi->rli->before_image_inconsistencies.load();
+  }
+  else
+    var->type= SHOW_UNDEF;
+
+  channel_map.unlock();
+  return 0;
+}
+
 /**
   This status variable is also exclusively (look comments on
   show_slave_running()) for default channel.
@@ -9025,6 +9047,9 @@ SHOW_VAR status_vars[] = {
      SHOW_SCOPE_GLOBAL},
 #endif
     {"Slave_running", (char *)&show_slave_running, SHOW_FUNC,
+     SHOW_SCOPE_GLOBAL},
+    {"Slave_before_image_inconsistencies",
+     (char*) &show_slave_before_image_inconsistencies, SHOW_FUNC,
      SHOW_SCOPE_GLOBAL},
     {"Slow_launch_threads",
      (char *)&Per_thread_connection_handler::slow_launch_threads, SHOW_LONG,

ldonoso/percona-server

sql/mysqld.cc
@@ -1349,6 +1349,7 @@ ulong binlog_checksum_options;
 ulong binlog_row_metadata;
 bool opt_source_verify_checksum = false;
 bool opt_replica_sql_verify_checksum = true;
+ulong opt_slave_check_before_image_consistency = 0;
 const char *binlog_format_names[] = {"MIXED", "STATEMENT", "ROW", NullS};
 bool binlog_gtid_simple_recovery;
 ulong binlog_error_action;
@@ -9535,6 +9536,23 @@ static int show_flushstatustime(THD *thd, SHOW_VAR *var, char *buff) {
 }
 #endif
 
+static int show_slave_before_image_inconsistencies(THD *, SHOW_VAR *var,
+                                                   char *buff) {
+  channel_map.rdlock();
+  Master_info *mi = channel_map.get_default_channel_mi();
+
+  if (mi && mi->rli && mi->rli->check_before_image_consistency) {
+    var->type = SHOW_LONGLONG;
+    var->value = buff;
+    *((ulonglong *)buff) =
+        (ulonglong)mi->rli->before_image_inconsistencies.load();
+  } else
+    var->type = SHOW_UNDEF;
+
+  channel_map.unlock();
+  return 0;
+}
+
 #ifndef NDEBUG
 static int show_replica_rows_last_search_algorithm_used(THD *, SHOW_VAR *var,
                                                         char *buff) {
@@ -10027,6 +10045,9 @@ SHOW_VAR status_vars[] = {
      (char *)&show_replica_rows_last_search_algorithm_used, SHOW_FUNC,
      SHOW_SCOPE_GLOBAL},
 #endif
+    {"Slave_before_image_inconsistencies",
+     (char *)&show_slave_before_image_inconsistencies, SHOW_FUNC,
+     SHOW_SCOPE_GLOBAL},
     {"Slave_high_priority_ddl_executed",
      (char *)&slave_high_priority_ddl_executed, SHOW_LONGLONG, SHOW_SCOPE_ALL},
     {"Slave_high_priority_ddl_killed_connections",