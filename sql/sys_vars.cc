oleksandr-kachan/mysql-5.6

FB8-263: Add JUnit reporting support to MTR
https://jira.percona.com/browse/FB8-263

This change is a combination of a few commits:

----- https://github.com/percona/percona-server/pull/3901 -----
Add JUnit reporting support to MTR

- Add JUnit reporting support to the MySQL test suite runner
- Add "junit" test suite with deliberately failing tests
- Add a deliberately failing unittests suite
- Attach mysqld shutdown and valgrind reports to JUnit report
- Run unit tests under Valgrind too if it was enabled
- Mark self-skipping tests as skipped in JUnit reports
- Convey tests output to JUnit reports regardless of test failure
- Exclude tests skipped by framework from results xml

----- https://github.com/percona/percona-server/pull/4246 -----
Improve communication between MTR test worker and main process

When MTR worker finishes its job it sends shutdown and valgrind
reports to main process. It exits right after that. Two issues
are possible at this point:
- main process may lose last sent data from a worker;
- from time to time main MTR ptocess may silently exit right
  after closing a worker.

To solve both these issues communication protocol between
main MTR process and workers is modified in this change.
The following changes are done:
- Worker shutdown process is split into two steps - collecting
  and sending shutdown/valgrind reports, actual worker process shutdown.
- The GETREPORTS message gets sent to a worker to make it collect and
  send shutdown reports. Worker process stays active at this point.
- Once main process receives all reports it sends BYE to a worker.
  Worker process exits after receiving this message.
Above changes make sure main MTR process has time to collect
all data from a worker before it exits.

----- https://github.com/percona/percona-server/pull/4361 -----
Select HEX encoded string when checking data in encrypted column

In case of innodb.import_compress_encrypt test failure it adds
substrings selected from encrypted VARCHAR column d

sql/sys_vars.cc
@@ -8691,3 +8691,7 @@ static Sys_var_enum Sys_sql_duplicate_executions_control(
     GLOBAL_VAR(sql_duplicate_executions_control), CMD_LINE(OPT_ARG),
     control_level_values, DEFAULT(CONTROL_LEVEL_OFF), NO_MUTEX_GUARD,
     NOT_IN_BINLOG, ON_CHECK(NULL), ON_UPDATE(NULL));
+
+#ifndef DBUG_OFF
+Debug_shutdown_actions Debug_shutdown_actions::instance;
+#endif

ldonoso/percona-server

sql/sys_vars.cc
@@ -7472,3 +7472,7 @@ static Sys_var_enum Sys_terminology_use_previous(
     terminology_use_previous_names, DEFAULT(terminology_use_previous::NONE),
     NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(nullptr), ON_UPDATE(nullptr),
     DEPRECATED_VAR(""));
+
+#ifndef NDEBUG
+Debug_shutdown_actions Debug_shutdown_actions::instance;
+#endif