facebook/mysql-5.6

Port Extra Stats: callers of os_file_flush
Summary:
Report on the callers of os_file_flush in SHOW INNODB STATUS.
This is displayed in the TABLESPACE section as 'fsync callers:'

Tihe diff is amended from 5.6 branch to remove DIRTY_BUFFER category as it
was not used and ARCHIVE category as I couldn't find a place to put it
in

Reference patch: https://github.com/facebook/mysql-5.6/commit/3e6bf37

Differential Revision: D6176814

fbshipit-source-id: 59251592b34

storage/innobase/buf/buf0dblwr.cc
@@ -100,7 +100,7 @@ void buf_dblwr_sync_datafiles() {
   os_aio_wait_until_no_pending_writes();
 
   /* Now we flush the data to disk (for example, with fsync) */
-  fil_flush_file_spaces(to_int(FIL_TYPE_TABLESPACE));
+  fil_flush_file_spaces(to_int(FIL_TYPE_TABLESPACE), FLUSH_FROM_DOUBLEWRITE);
 }
 
 /** Creates or initialializes the doublewrite buffer at a database start. */
@@ -614,7 +614,7 @@ void buf_dblwr_process() {
 
   dblwr.pages.clear();
 
-  fil_flush_file_spaces(to_int(FIL_TYPE_TABLESPACE));
+  fil_flush_file_spaces(to_int(FIL_TYPE_TABLESPACE), FLUSH_FROM_DOUBLEWRITE);
 }
 
 /** Recover pages from the double write buffer for a specific tablespace.
@@ -646,7 +646,7 @@ void buf_dblwr_recover_pages(fil_space_t *space) {
     }
   }
 
-  fil_flush_file_spaces(to_int(FIL_TYPE_TABLESPACE));
+  fil_flush_file_spaces(to_int(FIL_TYPE_TABLESPACE), FLUSH_FROM_DOUBLEWRITE);
 }
 
 /** Frees doublewrite buffer. */
@@ -698,7 +698,8 @@ void buf_dblwr_update(
         mutex_exit(&buf_dblwr->mutex);
         /* This will finish the batch. Sync data files
         to the disk. */
-        fil_flush_file_spaces(to_int(FIL_TYPE_TABLESPACE));
+        fil_flush_file_spaces(to_int(FIL_TYPE_TABLESPACE),
+                              FLUSH_FROM_DOUBLEWRITE);
         mutex_enter(&buf_dblwr->mutex);
 
         /* We can now reuse the doublewrite memory buffer: */
@@ -991,7 +992,7 @@ flush:
   srv_stats.dblwr_writes.inc();
 
   /* Now flush the doublewrite buffer data to disk */
-  fil_flush(TRX_SYS_SPACE);
+  fil_flush(TRX_SYS_SPACE, FLUSH_FROM_DOUBLEWRITE);
 
   /* We know that the writes have been flushed to disk now
   and in recovery we will find them in the doublewrite buffer
@@ -1206,7 +1207,7 @@ retry:
   ut_a(err == DB_SUCCESS);
 
   /* Now flush the doublewrite buffer data to disk */
-  fil_flush(TRX_SYS_SPACE);
+  fil_flush(TRX_SYS_SPACE, FLUSH_FROM_DOUBLEWRITE);
 
   /* We know that the write has been flushed to disk now
   and during recovery we will find it in the doublewrite buffer

ldonoso/percona-server

storage/innobase/buf/buf0dblwr.cc
@@ -872,6 +872,8 @@ void Double_write::prepare(const buf_page_t *bpage, void **ptr,
 
     *len = bpage->size.logical();
   }
+
+  fil_flush_file_spaces(to_int(FIL_TYPE_TABLESPACE), FLUSH_FROM_DOUBLEWRITE);
 }
 
 void Double_write::single_write(Segment *segment, const buf_page_t *bpage,
@@ -1152,7 +1154,7 @@ dberr_t Double_write::sync_page_flush(buf_page_t *bpage,
   auto err = write_to_datafile(bpage, true, e_block);
 
   if (err == DB_SUCCESS) {
-    fil_flush(bpage->id.space());
+    fil_flush(bpage->id.space(), FLUSH_FROM_DOUBLEWRITE);
   } else {
     /* This block is not freed if the write_to_datafile doesn't succeed. */
     if (e_block != nullptr) {
@@ -1729,7 +1731,7 @@ dberr_t dblwr::write(buf_flush_t flush_type, buf_page_t *bpage,
       ut_ad(flush_type == BUF_FLUSH_LRU || flush_type == BUF_FLUSH_SINGLE_PAGE);
 
       if (err == DB_SUCCESS) {
-        fil_flush(space_id);
+        fil_flush(space_id, FLUSH_FROM_DOUBLEWRITE);
       }
       /* true means we want to evict this page from the LRU list as well. */
       buf_page_io_complete(bpage, true);
@@ -1795,7 +1797,7 @@ void Double_write::write_complete(buf_page_t *bpage,
                               ? Double_write::s_LRU_batch_segments
                               : Double_write::s_flush_list_batch_segments;
 
-          fil_flush_file_spaces(FIL_TYPE_TABLESPACE);
+          fil_flush_file_spaces(FIL_TYPE_TABLESPACE, FLUSH_FROM_DOUBLEWRITE);
 
           while (!segments->enqueue(batch_segment)) {
             std::this_thread::yield();
@@ -2292,7 +2294,7 @@ void recv::Pages::recover(fil_space_t *space) noexcept {
         dblwr_recover_page(page->m_no, space, page_no, page->m_buffer.begin());
   }
 
-  fil_flush_file_spaces(FIL_TYPE_TABLESPACE);
+  fil_flush_file_spaces(FIL_TYPE_TABLESPACE, FLUSH_FROM_DOUBLEWRITE);
 #endif /* !UNIV_HOTBACKUP */
 }
 