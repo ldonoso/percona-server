inikep/mysql-5.6

Make MyRocks 8.0 buildable [non-RocksDB part]
Summary:
List of changes that makes MyRocks 8 buildable:

1. Need to allow undefined symbols (from TP2) as well as disabling a few other compiler warnings. TODO: revisit and fix up code
2. Port PLUGIN_VAR_ALLOCATED partially to allow building
3. Port missing error codes to errmsg-utf8.txt - care is taken to ensure the error numbers are correct (but not so much for ER_INVALID_ADMIN_ADDRESS/etc - should follow up
4. Header files have been changed/moved
5. Port DB_TYPE_LEVELDB / DB_TYPE_ROCKSDB partially
6. Port is_full_key helpers 85e5baf2eed5
7. Disabled slave GTID
8. Move MYSQL_TABLE_IO_WAIT to public sql/handler.h and made a few members public. TODO: is there a better way?
9. Use thd_query_safe / thd_query_unsafe API instead of thd->query
10. Made TABLE::m_status public. TODO: is there a better way?
11. Copied atomic_stat.h from 5.6 to 8.0 as the file is missing in 8.0
12. Changed my_bool (gone in 8.0) to bool, and TRUE/FALSE to true/false
13. Commented out handler::set_force_skip_unique_check. TODO: port
c0e6859bffd Add skip unique check whitelist and new debug_skip_unique_check
14.  ha_rocksdb::index_type & ha_rocksdb::has_ext is removed - they are only for SHOW commands and removed in 8.0
15. HA_REC_NOT_IN_SEQ & HA_ONLINE_ANALYZE is commented out as they are gone in 8.0
16. Ihandler::init_with_fields override is commented out - TODO: port 070a257a1c3 Issue #108: Index-only scans do not work for partitioned tables and extended keys.
17. Handler::keys_to_use_for_scanning is gone in 8.0. TODO: does this impact any behavior?
18. Many handler overrides now take dd:Table
19. Handler::register_query_cache_table is no longer a override in 8.0. We don't support it anyway
20. Handle read free replication API hook are commented out. TODO: port 019a9fcd7f6 Add Tokutek's read-free replication api in mysqld, af531c246d35 New variable to control read free replication
21. has_ttl_column commented out. TODO: port 92a364f86100 Ignore 

sql/handler.h
@@ -61,7 +61,9 @@
 #include "my_thread_local.h"  // my_errno
 #include "mysql/components/services/psi_table_bits.h"
 #include "nullable.h"          // Nullable
-#include "sql/dd/object_id.h"  // dd::Object_id
+#include "mysql/psi/mysql_rwlock.h"
+#include "sql/dd/object_id.h"   // dd::Object_id
+#include "sql/dd/properties.h"  // dd::Properties
 #include "sql/dd/string_type.h"
 #include "sql/dd/types/object_table.h"  // dd::Object_table
 #include "sql/discrete_interval.h"      // Discrete_interval
@@ -4211,7 +4213,8 @@ class handler {
   std::mt19937 m_random_number_engine;
   double m_sampling_percentage;
 
- private:
+ /* TODO(yzha) - we needed these to be public for MYSQL_TABLE_IO_WAIT */
+ public:
   /** Internal state of the batch instrumentation. */
   enum batch_mode_t {
     /** Batch mode not used. */
@@ -7054,5 +7057,61 @@ std::unordered_set<std::string> split_into_set(const std::string &input,
                                                char delimiter);
 std::vector<std::string> split_into_vector(const std::string &input,
                                            char delimiter);
+
+/**
+  @def MYSQL_TABLE_IO_WAIT
+  Instrumentation helper for table io_waits.
+  Note that this helper is intended to be used from
+  within the handler class only, as it uses members
+  from @c handler
+  Performance schema events are instrumented as follows:
+  - in non batch mode, one event is generated per call
+  - in batch mode, the number of rows affected is saved
+  in @c m_psi_numrows, so that @c end_psi_batch_mode()
+  generates a single event for the batch.
+  @param OP the table operation to be performed
+  @param INDEX the table index used if any, or MAX_KEY.
+  @param RESULT the result of the table operation performed
+  @param PAYLOAD instrumented code to execute
+  @sa handler::end_psi_batch_mode.
+*/
+#ifdef HAVE_PSI_TABLE_INTERFACE
+#define MYSQL_TABLE_IO_WAIT(OP, INDEX, RESULT, PAYLOAD)                     \
+  {                                                                         \
+    if (m_psi != NULL) {                                                    \
+      switch (m_psi_batch_mode) {                                           \
+        case PSI_BATCH_MODE_NONE: {                                         \
+          PSI_table_locker *sub_locker = NULL;                              \
+          PSI_table_locker_state reentrant_safe_state;                      \
+          sub_locker = PSI_TABLE_CALL(start_table_io_wait)(                 \
+              &reentrant_safe_state, m_psi, OP, INDEX, __FILE__, __LINE__); \
+          PAYLOAD                                                           \
+          if (sub_locker != NULL) PSI_TABLE_CALL(end_table_io_wait)         \
+          (sub_locker, 1);                                                  \
+          break;                                                            \
+        }                                                                   \
+        case PSI_BATCH_MODE_STARTING: {                                     \
+          m_psi_locker = PSI_TABLE_CALL(start_table_io_wait)(               \
+              &m_psi_locker_state, m_psi, OP, INDEX, __FILE__, __LINE__);   \
+          PAYLOAD                                                           \
+          if (!RESULT) m_psi_numrows++;                                     \
+          m_psi_batch_mode = PSI_BATCH_MODE_STARTED;                        \
+          break;                                                            \
+        }                                                                   \
+        case PSI_BATCH_MODE_STARTED:                                        \
+        default: {                                                          \
+          DBUG_ASSERT(m_psi_batch_mode == PSI_BATCH_MODE_STARTED);          \
+          PAYLOAD                                                           \
+          if (!RESULT) m_psi_numrows++;                                     \
+          break;                                                            \
+        }                                                                   \
+      }                                                                     \
+    } else {                                                                \
+      PAYLOAD                                                               \
+    }                                                                       \
+  }
+#else
+#define MYSQL_TABLE_IO_WAIT(OP, INDEX, RESULT, PAYLOAD) PAYLOAD
+#endif
 
 #endif /* HANDLER_INCLUDED */

ldonoso/percona-server

sql/handler.h
@@ -61,7 +61,9 @@
 #include "my_table_map.h"
 #include "my_thread_local.h"  // my_errno
 #include "mysql/components/services/psi_table_bits.h"
-#include "sql/dd/object_id.h"  // dd::Object_id
+#include "mysql/psi/mysql_rwlock.h"
+#include "sql/dd/object_id.h"   // dd::Object_id
+#include "sql/dd/properties.h"  // dd::Properties
 #include "sql/dd/string_type.h"
 #include "sql/dd/types/object_table.h"  // dd::Object_table
 #include "sql/discrete_interval.h"      // Discrete_interval
@@ -4301,7 +4303,8 @@ class handler {
   std::mt19937 m_random_number_engine;
   double m_sampling_percentage;
 
- private:
+  /* TODO(yzha) - we needed these to be public for MYSQL_TABLE_IO_WAIT */
+ public:
   /** Internal state of the batch instrumentation. */
   enum batch_mode_t {
     /** Batch mode not used. */
@@ -7186,5 +7189,63 @@ std::unordered_set<std::string> split_into_set(const std::string &input,
                                                char delimiter);
 std::vector<std::string> split_into_vector(const std::string &input,
                                            char delimiter);
+
+/**
+  @def MYSQL_TABLE_IO_WAIT
+  Instrumentation helper for table io_waits.
+  Note that this helper is intended to be used from
+  within the handler class only, as it uses members
+  from @c handler
+  Performance schema events are instrumented as follows:
+  - in non batch mode, one event is generated per call
+  - in batch mode, the number of rows affected is saved
+  in @c m_psi_numrows, so that @c end_psi_batch_mode()
+  generates a single event for the batch.
+  @param OP the table operation to be performed
+  @param INDEX the table index used if any, or MAX_KEY.
+  @param RESULT the result of the table operation performed
+  @param PAYLOAD instrumented code to execute
+  @sa handler::end_psi_batch_mode.
+*/
+#ifdef HAVE_PSI_TABLE_INTERFACE
+#define MYSQL_TABLE_IO_WAIT(OP, INDEX, RESULT, PAYLOAD)                     \
+  {                                                                         \
+    if (m_psi != NULL) {                                                    \
+      switch (m_psi_batch_mode) {                                           \
+        case PSI_BATCH_MODE_NONE: {                                         \
+          PSI_table_locker *sub_locker = NULL;                              \
+          PSI_table_locker_state reentrant_safe_state;                      \
+          reentrant_safe_state.m_thread = nullptr;                          \
+          reentrant_safe_state.m_wait = nullptr;                            \
+          sub_locker = PSI_TABLE_CALL(start_table_io_wait)(                 \
+              &reentrant_safe_state, m_psi, OP, INDEX, __FILE__, __LINE__); \
+          PAYLOAD                                                           \
+          if (sub_locker != NULL) PSI_TABLE_CALL(end_table_io_wait)         \
+          (sub_locker, 1);                                                  \
+          break;                                                            \
+        }                                                                   \
+        case PSI_BATCH_MODE_STARTING: {                                     \
+          m_psi_locker = PSI_TABLE_CALL(start_table_io_wait)(               \
+              &m_psi_locker_state, m_psi, OP, INDEX, __FILE__, __LINE__);   \
+          PAYLOAD                                                           \
+          if (!RESULT) m_psi_numrows++;                                     \
+          m_psi_batch_mode = PSI_BATCH_MODE_STARTED;                        \
+          break;                                                            \
+        }                                                                   \
+        case PSI_BATCH_MODE_STARTED:                                        \
+        default: {                                                          \
+          assert(m_psi_batch_mode == PSI_BATCH_MODE_STARTED);               \
+          PAYLOAD                                                           \
+          if (!RESULT) m_psi_numrows++;                                     \
+          break;                                                            \
+        }                                                                   \
+      }                                                                     \
+    } else {                                                                \
+      PAYLOAD                                                               \
+    }                                                                       \
+  }
+#else
+#define MYSQL_TABLE_IO_WAIT(OP, INDEX, RESULT, PAYLOAD) PAYLOAD
+#endif
 
 #endif /* HANDLER_INCLUDED */