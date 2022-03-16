facebook/mysql-5.6

port 5.6 json_contains implementation to 8.0
Summary:
The change ports 5.6 implementation of json_contains to 8.0. Since 8.0
already contains json_contains, session variable use_fb_json_functions is
modified to take in use_fb_json_contains switch as well. When set to true,
json_contains simulates 5.6 behavior.

Internally the code to implement json_contains creates a path expression
'$**.<key>' to search for all the occurrences of the key in the document. In
case the matches are found, the value is compared for equality if the value
was passed in as argument. It is possible that multiple matches are found.
So each of the value is compared to determine if any key contains the value.

5.6 json_contains implementation iterates over all the duplicate keys and
returns true if either of the duplicate key contains the value. Since
legacy Json_object contains a multimap,  no changes to Json_wrapper::seek
are needed.

Had to change Json_wrapper::compare to enable legacy comparison. In
5.6 boolean is comparable against ints.

Reviewed By: hermanlee

Differential Revision: D23157502

fbshipit-source-id: 85554c79951

sql/json_dom.cc
@@ -2600,6 +2600,19 @@ static int compare_json_strings(const char *str1, size_t str1_len,
 static constexpr int num_json_types =
     static_cast<int>(enum_json_type::J_ERROR) + 1;
 
+static bool legacy_value_type_number(enum_json_type type) {
+  switch (type) {
+    case enum_json_type::J_BOOLEAN:
+    case enum_json_type::J_UINT:
+    case enum_json_type::J_INT:
+    case enum_json_type::J_DOUBLE:
+    case enum_json_type::J_DECIMAL:
+      return true;
+    default:
+      return false;
+  }
+}
+
 /**
   The following matrix tells how two JSON values should be compared
   based on their types. If type_comparison[type_of_a][type_of_b] is
@@ -2627,8 +2640,8 @@ static constexpr int type_comparison[num_json_types][num_json_types] = {
 };
 // clang-format on
 
-int Json_wrapper::compare(const Json_wrapper &other,
-                          const CHARSET_INFO *cs) const {
+int Json_wrapper::compare(const Json_wrapper &other, const CHARSET_INFO *cs,
+                          bool legacy_val_cmp) const {
   const enum_json_type this_type = type();
   const enum_json_type other_type = other.type();
 
@@ -2638,6 +2651,10 @@ int Json_wrapper::compare(const Json_wrapper &other,
   // Check if the type tells us which value is bigger.
   int cmp = type_comparison[static_cast<int>(this_type)]
                            [static_cast<int>(other_type)];
+  if (legacy_val_cmp && legacy_value_type_number(this_type) &&
+      legacy_value_type_number(other_type)) {
+    cmp = 0;
+  }
   if (cmp != 0) return cmp;
 
   // Same or similar type. Go on and inspect the values.
@@ -2737,6 +2754,9 @@ int Json_wrapper::compare(const Json_wrapper &other,
           if (other.get_decimal_data(&b_dec)) return 1; /* purecov: inspected */
           return -compare_json_decimal_int(b_dec, get_int());
         }
+        case enum_json_type::J_BOOLEAN:
+          DBUG_ASSERT(legacy_val_cmp);
+          return compare_numbers(get_int(), (longlong)other.get_boolean());
         default:;
       }
       break;
@@ -2754,6 +2774,9 @@ int Json_wrapper::compare(const Json_wrapper &other,
           if (other.get_decimal_data(&b_dec)) return 1; /* purecov: inspected */
           return -compare_json_decimal_uint(b_dec, get_uint());
         }
+        case enum_json_type::J_BOOLEAN:
+          DBUG_ASSERT(legacy_val_cmp);
+          return -compare_json_int_uint(other.get_boolean(), get_uint());
         default:;
       }
       break;
@@ -2772,6 +2795,9 @@ int Json_wrapper::compare(const Json_wrapper &other,
             return 1; /* purecov: inspected */
           return -compare_json_decimal_double(other_dec, get_double());
         }
+        case enum_json_type::J_BOOLEAN:
+          DBUG_ASSERT(legacy_val_cmp);
+          return compare_json_double_int(get_double(), other.get_boolean());
         default:;
       }
       break;
@@ -2798,13 +2824,36 @@ int Json_wrapper::compare(const Json_wrapper &other,
             return compare_json_decimal_uint(a_dec, other.get_uint());
           case enum_json_type::J_DOUBLE:
             return compare_json_decimal_double(a_dec, other.get_double());
+          case enum_json_type::J_BOOLEAN:
+            DBUG_ASSERT(legacy_val_cmp);
+            return compare_json_decimal_int(a_dec, other.get_boolean());
           default:;
         }
         break;
       }
     case enum_json_type::J_BOOLEAN:
       // Booleans are only equal to other booleans. false is less than true.
-      return compare_numbers(get_boolean(), other.get_boolean());
+      if (other_type == enum_json_type::J_BOOLEAN) {
+        return compare_numbers(get_boolean(), other.get_boolean());
+      } else {
+        DBUG_ASSERT(legacy_val_cmp);
+        switch (other_type) {
+          case enum_json_type::J_INT:
+            return compare_numbers((longlong)get_boolean(), other.get_int());
+          case enum_json_type::J_UINT:
+            return compare_json_int_uint(get_boolean(), other.get_uint());
+          case enum_json_type::J_DOUBLE:
+            return -compare_json_double_int(other.get_double(), get_boolean());
+          case enum_json_type::J_DECIMAL: {
+            my_decimal b_dec;
+            if (other.get_decimal_data(&b_dec)) return 1;
+            return -compare_json_decimal_int(b_dec, get_boolean());
+          }
+          default:
+            DBUG_ASSERT(false);
+        }
+      }
+      break;
     case enum_json_type::J_DATETIME:
     case enum_json_type::J_TIMESTAMP:
       // Timestamps and datetimes can be equal to each other.

ldonoso/percona-server

sql/json_dom.cc
@@ -2534,6 +2534,19 @@ static int compare_json_strings(const char *str1, size_t str1_len,
 static constexpr int num_json_types =
     static_cast<int>(enum_json_type::J_ERROR) + 1;
 
+static bool legacy_value_type_number(enum_json_type type) {
+  switch (type) {
+    case enum_json_type::J_BOOLEAN:
+    case enum_json_type::J_UINT:
+    case enum_json_type::J_INT:
+    case enum_json_type::J_DOUBLE:
+    case enum_json_type::J_DECIMAL:
+      return true;
+    default:
+      return false;
+  }
+}
+
 /**
   The following matrix tells how two JSON values should be compared
   based on their types. If type_comparison[type_of_a][type_of_b] is
@@ -2561,8 +2574,8 @@ static constexpr int type_comparison[num_json_types][num_json_types] = {
 };
 // clang-format on
 
-int Json_wrapper::compare(const Json_wrapper &other,
-                          const CHARSET_INFO *cs) const {
+int Json_wrapper::compare(const Json_wrapper &other, const CHARSET_INFO *cs,
+                          bool legacy_val_cmp) const {
   const enum_json_type this_type = type();
   const enum_json_type other_type = other.type();
 
@@ -2572,6 +2585,10 @@ int Json_wrapper::compare(const Json_wrapper &other,
   // Check if the type tells us which value is bigger.
   int type_cmp = type_comparison[static_cast<int>(this_type)]
                                 [static_cast<int>(other_type)];
+  if (legacy_val_cmp && legacy_value_type_number(this_type) &&
+      legacy_value_type_number(other_type)) {
+    type_cmp = 0;
+  }
   if (type_cmp != 0) return type_cmp;
 
   // Same or similar type. Go on and inspect the values.
@@ -2671,6 +2688,9 @@ int Json_wrapper::compare(const Json_wrapper &other,
           if (other.get_decimal_data(&b_dec)) return 1; /* purecov: inspected */
           return -compare_json_decimal_int(b_dec, get_int());
         }
+        case enum_json_type::J_BOOLEAN:
+          assert(legacy_val_cmp);
+          return compare_numbers(get_int(), (longlong)other.get_boolean());
         default:;
       }
       break;
@@ -2688,6 +2708,9 @@ int Json_wrapper::compare(const Json_wrapper &other,
           if (other.get_decimal_data(&b_dec)) return 1; /* purecov: inspected */
           return -compare_json_decimal_uint(b_dec, get_uint());
         }
+        case enum_json_type::J_BOOLEAN:
+          assert(legacy_val_cmp);
+          return -compare_json_int_uint(other.get_boolean(), get_uint());
         default:;
       }
       break;
@@ -2706,6 +2729,9 @@ int Json_wrapper::compare(const Json_wrapper &other,
             return 1; /* purecov: inspected */
           return -compare_json_decimal_double(other_dec, get_double());
         }
+        case enum_json_type::J_BOOLEAN:
+          assert(legacy_val_cmp);
+          return compare_json_double_int(get_double(), other.get_boolean());
         default:;
       }
       break;
@@ -2732,13 +2758,36 @@ int Json_wrapper::compare(const Json_wrapper &other,
             return compare_json_decimal_uint(a_dec, other.get_uint());
           case enum_json_type::J_DOUBLE:
             return compare_json_decimal_double(a_dec, other.get_double());
+          case enum_json_type::J_BOOLEAN:
+            assert(legacy_val_cmp);
+            return compare_json_decimal_int(a_dec, other.get_boolean());
           default:;
         }
         break;
       }
     case enum_json_type::J_BOOLEAN:
       // Booleans are only equal to other booleans. false is less than true.
-      return compare_numbers(get_boolean(), other.get_boolean());
+      if (other_type == enum_json_type::J_BOOLEAN) {
+        return compare_numbers(get_boolean(), other.get_boolean());
+      } else {
+        assert(legacy_val_cmp);
+        switch (other_type) {
+          case enum_json_type::J_INT:
+            return compare_numbers((longlong)get_boolean(), other.get_int());
+          case enum_json_type::J_UINT:
+            return compare_json_int_uint(get_boolean(), other.get_uint());
+          case enum_json_type::J_DOUBLE:
+            return -compare_json_double_int(other.get_double(), get_boolean());
+          case enum_json_type::J_DECIMAL: {
+            my_decimal b_dec;
+            if (other.get_decimal_data(&b_dec)) return 1;
+            return -compare_json_decimal_int(b_dec, get_boolean());
+          }
+          default:
+            assert(false);
+        }
+      }
+      break;
     case enum_json_type::J_DATETIME:
     case enum_json_type::J_TIMESTAMP:
       // Timestamps and datetimes can be equal to each other.