facebook/mysql-5.6

FB8-59: Database admission control (#969) (#969)
Summary:
JIRA: https://jira.percona.com/browse/FB8-59

Reference patch: https://github.com/facebook/mysql-5.6/commit/5cbfab0
Reference patch: https://github.com/facebook/mysql-5.6/commit/0ebaf75
Reference patch: https://github.com/facebook/mysql-5.6/commit/cf7ce48
Reference patch: https://github.com/facebook/mysql-5.6/commit/3842a8f
Reference patch: https://github.com/facebook/mysql-5.6/commit/bcbd77a
Reference patch: https://github.com/facebook/mysql-5.6/commit/e6c2b87
Reference patch: https://github.com/facebook/mysql-5.6/commit/928c2a5
Reference patch: https://github.com/facebook/mysql-5.6/commit/86587af
Reference patch: https://github.com/facebook/mysql-5.6/commit/7870828
Reference patch: https://github.com/facebook/mysql-5.6/commit/0151b45
Reference patch: https://github.com/facebook/mysql-5.6/commit/75de821
Reference patch: https://github.com/facebook/mysql-5.6/commit/08d4174
Reference patch: https://github.com/facebook/mysql-5.6/commit/c1e5b4f
Reference patch: https://github.com/facebook/mysql-5.6/commit/08368d4
Reference patch: https://github.com/facebook/mysql-5.6/commit/a2198e0
Reference patch: https://github.com/facebook/mysql-5.6/commit/0730342
Reference patch: https://github.com/facebook/mysql-5.6/commit/d868c1b
Reference patch: https://github.com/facebook/mysql-5.6/commit/645fc5e
Reference patch: https://github.com/facebook/mysql-5.6/commit/0b130f1ebc9
Reference patch: https://github.com/facebook/mysql-5.6/commit/f49a4a74945

<Comment by Percona developers>

This patch ports Database admission control. The multi-tenancy plugin
changes are not ported. This code will be eventually moved to new plugin
of audit type.

There are possible improvements with code. Below are the deferred items
(makes it easier for review. Can compare with 5.6 code)

1. Remove MT_RESOURCE_ATTRS. we only need database name (also removes the typedefs)
2. Remove enums enum_multi_tenancy_resource_type & enum_multi_tenancy_return_type

mysql-test/t/admission_control_multi_query.py
@@ -1,0 +1,121 @@
+import time
+import sys
+import MySQLdb
+import argparse
+import random
+import threading
+import traceback
+
+NUM_WORKERS = 50
+NUM_TRANSACTIONS = 10000
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        'multi_query',
+        description='Generate Multi query load',
+    )
+    parser.add_argument(
+        '--host',
+        type=str,
+        help='host name')
+    parser.add_argument(
+        '--port',
+        type=int,
+        help='port number')
+    parser.add_argument(
+        '--user',
+        type=str,
+        help='user name')
+    parser.add_argument(
+        '--password',
+        default='',
+        type=str,
+        help='password')
+    parser.add_argument(
+        '--database',
+        type=str,
+        help='database to use')
+
+    return parser.parse_args()
+
+def generate_load(args):
+    con = MySQLdb.connect(user=args.user,
+                          passwd=args.password,
+                          host=args.host,
+                          port=args.port,
+                          db=args.database)
+    for i in xrange(NUM_TRANSACTIONS):
+        cursor = con.cursor()
+        cursor.execute('begin;')
+        sql = ''
+        values = []
+        for i in xrange(3):
+            val = random.randrange(1, 50)
+            values.append(val)
+        values = sorted(values)
+        for val in values:
+            sql += 'insert into t1 values(%d, 1) on duplicate key ' \
+                   'update b=greatest(b+1, 0);' % (val)
+        sql += 'commit;'
+        cursor.execute(sql)
+        cursor.close()
+    con.close()
+
+def run_admin_checks(args):
+    con = MySQLdb.connect(user=args.user,
+                          passwd=args.password,
+                          host=args.host,
+                          port=args.port,
+                          db=args.database)
+    cursor=con.cursor()
+    cursor.execute("select @@global.max_running_queries")
+    rows = cursor.fetchone()
+    max_running_queries = int(rows[0])
+    for i in xrange(NUM_TRANSACTIONS):
+        cursor=con.cursor()
+        cursor.execute("show status like '%admission%'")
+        rows = cursor.fetchall()
+        if int(rows[1][1]) > max_running_queries:
+            raise Exception('Current running queries %s is more than ' \
+                            'max_running_queries %d' % (rows[1][1],
+                             max_running_queries))
+
+class worker_thread(threading.Thread):
+    def __init__(self, args, admin):
+        threading.Thread.__init__(self)
+        self.args = args
+        self.exception = None
+        self.admin = admin
+        self.start()
+
+    def run(self):
+        try:
+            if self.admin:
+                run_admin_checks(self.args)
+            else:
+                generate_load(self.args)
+        except Exception, e:
+            self.exception = traceback.format_exc()
+
+def main():
+    args = parse_args()
+    workers = []
+    for i in xrange(NUM_WORKERS):
+        worker = worker_thread(args, False)
+        workers.append(worker)
+
+    admin_worker = worker_thread(args, True)
+    workers.append(admin_worker)
+
+    worker_failed = False
+    for w in workers:
+        w.join()
+        if w.exception:
+            print "worker hit an exception:\n%s\n'" % w.exception
+            worker_failed = True
+
+    if worker_failed:
+        sys.exit(1)
+
+if __name__ == '__main__':
+  main()

sql/sql_db.cc
@@ -87,7 +87,8 @@
 #include "sql/rpl_gtid.h"
 #include "sql/rpl_slave_commit_order_manager.h"  // Commit_order_manager
 #include "sql/session_tracker.h"
-#include "sql/sp.h"         // lock_db_routines
+#include "sql/sp.h"  // lock_db_routines
+#include "sql/sql_admission_control.h"
 #include "sql/sql_base.h"   // lock_table_names
 #include "sql/sql_class.h"  // THD
 #include "sql/sql_const.h"
@@ -999,6 +1000,10 @@ bool mysql_rm_db(THD *thd, const LEX_CSTRING &db, bool if_exists) {
     }
   }
 
+  if (!error) {
+    db_ac->remove(db.str);
+  }
+
   thd->server_status |= SERVER_STATUS_DB_DROPPED;
   my_ok(thd, deleted_tables);
   return false;
@@ -1260,6 +1265,17 @@ err:
 static void mysql_change_db_impl(THD *thd, const LEX_CSTRING &new_db_name,
                                  ulong new_db_access,
                                  const CHARSET_INFO *new_db_charset) {
+  if (thd->is_in_ac) {
+    // current admission control is per database
+    DBUG_ASSERT(thd->db().str != nullptr);
+    // if the thd is already in admission control for the previous
+    // database, we need to release it before resetting the value.
+    MT_RESOURCE_ATTRS attrs = {nullptr, nullptr, nullptr};
+    attrs.database = thd->db().str;
+    multi_tenancy_exit_query(thd, &attrs);
+    thd->is_in_ac = false;
+  }
+
   /* 1. Change current database in THD. */
 
   if (new_db_name.str == nullptr) {

ldonoso/percona-server

mysql-test/t/admission_control_multi_query.py
@@ -1,0 +1,121 @@
+import time
+import sys
+import MySQLdb
+import argparse
+import random
+import threading
+import traceback
+
+NUM_WORKERS = 50
+NUM_TRANSACTIONS = 2500
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        'multi_query',
+        description='Generate Multi query load',
+    )
+    parser.add_argument(
+        '--host',
+        type=str,
+        help='host name')
+    parser.add_argument(
+        '--port',
+        type=int,
+        help='port number')
+    parser.add_argument(
+        '--user',
+        type=str,
+        help='user name')
+    parser.add_argument(
+        '--password',
+        default='',
+        type=str,
+        help='password')
+    parser.add_argument(
+        '--database',
+        type=str,
+        help='database to use')
+
+    return parser.parse_args()
+
+def generate_load(args):
+    con = MySQLdb.connect(user=args.user,
+                          passwd=args.password,
+                          host=args.host,
+                          port=args.port,
+                          db=args.database)
+    for i in range(NUM_TRANSACTIONS):
+        cursor = con.cursor()
+        cursor.execute('begin;')
+        sql = ''
+        values = []
+        for i in range(3):
+            val = random.randrange(1, 50)
+            values.append(val)
+        values = sorted(values)
+        for val in values:
+            sql += 'insert into t1 values(%d, 1) on duplicate key ' \
+                   'update b=greatest(b+1, 0);' % (val)
+        sql += 'commit;'
+        cursor.execute(sql)
+        cursor.close()
+    con.close()
+
+def run_admin_checks(args):
+    con = MySQLdb.connect(user=args.user,
+                          passwd=args.password,
+                          host=args.host,
+                          port=args.port,
+                          db=args.database)
+    cursor=con.cursor()
+    cursor.execute("select @@global.max_running_queries")
+    rows = cursor.fetchone()
+    max_running_queries = int(rows[0])
+    for i in range(NUM_TRANSACTIONS):
+        cursor=con.cursor()
+        cursor.execute("show status like '%admission%'")
+        rows = cursor.fetchall()
+        if int(rows[1][1]) > max_running_queries:
+            raise Exception('Current running queries %s is more than ' \
+                            'max_running_queries %d' % (rows[1][1],
+                             max_running_queries))
+
+class worker_thread(threading.Thread):
+    def __init__(self, args, admin):
+        threading.Thread.__init__(self)
+        self.args = args
+        self.exception = None
+        self.admin = admin
+        self.start()
+
+    def run(self):
+        try:
+            if self.admin:
+                run_admin_checks(self.args)
+            else:
+                generate_load(self.args)
+        except Exception as e:
+            self.exception = traceback.format_exc()
+
+def main():
+    args = parse_args()
+    workers = []
+    for i in range(NUM_WORKERS):
+        worker = worker_thread(args, False)
+        workers.append(worker)
+
+    admin_worker = worker_thread(args, True)
+    workers.append(admin_worker)
+
+    worker_failed = False
+    for w in workers:
+        w.join()
+        if w.exception:
+            print("worker hit an exception:\n%s\n'" % w.exception)
+            worker_failed = True
+
+    if worker_failed:
+        sys.exit(1)
+
+if __name__ == '__main__':
+  main()

sql/sql_db.cc
@@ -87,7 +87,8 @@
 #include "sql/rpl_gtid.h"
 #include "sql/rpl_replica_commit_order_manager.h"  // Commit_order_manager
 #include "sql/session_tracker.h"
-#include "sql/sp.h"         // lock_db_routines
+#include "sql/sp.h"  // lock_db_routines
+#include "sql/sql_admission_control.h"
 #include "sql/sql_base.h"   // lock_table_names
 #include "sql/sql_class.h"  // THD
 #include "sql/sql_const.h"
@@ -1124,6 +1125,10 @@ bool mysql_rm_db(THD *thd, const LEX_CSTRING &db, bool if_exists) {
     }
   }
 
+  if (!error) {
+    db_ac->remove(db.str);
+  }
+
   thd->server_status |= SERVER_STATUS_DB_DROPPED;
   my_ok(thd, deleted_tables);
   return false;
@@ -1385,6 +1390,17 @@ err:
 static void mysql_change_db_impl(THD *thd, const LEX_CSTRING &new_db_name,
                                  ulong new_db_access,
                                  const CHARSET_INFO *new_db_charset) {
+  if (thd->is_in_ac) {
+    // current admission control is per database
+    assert(thd->db().str != nullptr);
+    // if the thd is already in admission control for the previous
+    // database, we need to release it before resetting the value.
+    MT_RESOURCE_ATTRS attrs = {nullptr, nullptr, nullptr};
+    attrs.database = thd->db().str;
+    multi_tenancy_exit_query(thd, &attrs);
+    thd->is_in_ac = false;
+  }
+
   /* 1. Change current database in THD. */
 
   if (new_db_name.str == nullptr) {