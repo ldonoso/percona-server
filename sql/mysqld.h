facebook/mysql-5.6

apply log related sysvars
Summary:
Bring in apply log related sysvars. This is almost a direct port from 5.6 myraft
branch with minor cosmetic changes.

This diff is just setting up the stack for adding promotion and master/slave
setup (which will be added later in the stack)

Reviewed By: anirbanr-fb

Differential Revision: D22181424

fbshipit-source-id: 9c6fa2816c4

sql/mysqld.h
@@ -411,6 +411,10 @@ extern ulonglong maximum_hlc_drift_ns;
 extern bool enable_raft_plugin;
 extern bool disallow_raft;
 
+/* Apply log related variables for raft */
+extern char *opt_apply_logname;
+extern char *opt_applylog_index_name;
+
 /* What should the server do when trxs fail inside ordered commit */
 extern ulong opt_commit_consensus_error_action;
 

ldonoso/percona-server

sql/mysqld.h
@@ -423,6 +423,10 @@ extern ulonglong maximum_hlc_drift_ns;
 extern bool enable_raft_plugin;
 extern bool disallow_raft;
 
+/* Apply log related variables for raft */
+extern char *opt_apply_logname_ptr;
+extern char *opt_applylog_index_name_ptr;
+
 /* What should the server do when trxs fail inside ordered commit */
 extern ulong opt_commit_consensus_error_action;
 