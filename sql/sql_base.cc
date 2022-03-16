facebook/mysql-5.6

Add high priority dll mtr test
Summary:
This change contains the high priority ddl test for task T33221077. High
priority dll is a FB internal feature and so this test is not included
in the original diff, which is going to be submitted to ORACLE.

Reference Patch: https://github.com/facebook/mysql-5.6/commit/bc44852de33

Reviewed By: hermanlee

Differential Revision: D13457834

fbshipit-source-id: 15f93b27f19

sql/sql_base.cc
@@ -3058,6 +3058,13 @@ bool open_table(THD *thd, TABLE_LIST *table_list, Open_table_context *ot_ctx) {
     mdl_ticket = table_list->mdl_request.ticket;
   }
 
+  DBUG_EXECUTE_IF("sql_opening_table", {
+    if (thd->slave_thread) {
+      const char act[] = "now signal opening wait_for slave_killed";
+      DBUG_ASSERT(!debug_sync_set_action(current_thd, STRING_WITH_LEN(act)));
+    }
+  });
+
   if (table_list->open_strategy == TABLE_LIST::OPEN_IF_EXISTS ||
       table_list->open_strategy == TABLE_LIST::OPEN_FOR_CREATE) {
     bool exists;

ldonoso/percona-server

sql/sql_base.cc
@@ -3089,6 +3089,13 @@ bool open_table(THD *thd, TABLE_LIST *table_list, Open_table_context *ot_ctx) {
     mdl_ticket = table_list->mdl_request.ticket;
   }
 
+  DBUG_EXECUTE_IF("sql_opening_table", {
+    if (thd->slave_thread) {
+      const char act[] = "now signal opening wait_for slave_killed";
+      assert(!debug_sync_set_action(current_thd, STRING_WITH_LEN(act)));
+    }
+  });
+
   if (table_list->open_strategy == TABLE_LIST::OPEN_IF_EXISTS ||
       table_list->open_strategy == TABLE_LIST::OPEN_FOR_CREATE) {
     bool exists;