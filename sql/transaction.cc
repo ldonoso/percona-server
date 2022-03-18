facebook/mysql-5.6

SQL to create and manage explicit snapshots (#1135) (#1135)
Summary:
This change introduces the following SQL statements:

- START TRANSACTION WITH SHARED <engine> SNAPSHOT - This works just like
transactions with consistent snapshot but the snapshot can be reused by other
transactions (see next SQL statement). The binlog pos, gtid executed and ID of
the snapshot is printed in the output.

- START TRANSACTION WITH EXISITING <engine> SNAPSHOT X - This works by using an
existing snapshot (created using the previous statement) instead of creating a
new one. X is the snapshot ID.

- CREATE EXPLICIT <engine> SNAPSHOT - This creates a snapshot of the current
state of the DB and associates it with the connection. The snapshot can be
reused by other connections. The output contains the binlog pos, gtid executed
and a snapshot ID. It works just like transaction with a shared snapshot but it
operates on the connection level and does not begin a transaction. After this
statment, all transactions in the connection will use the created snapshot until
it is released.

- ATTACH EXPLICIT <engine> SNAPSHOT X - This is analogous to transaction with
existing snapshot but operates on the connection level. X is the snapshot ID.

- RELEASE EXPLICIT <engine> SNAPSHOT - This releases the snapshot associated with
the connection. After this statment, all transactions in the connection will
create their own snapshots.

MyRocks support for the following SQL statements

START TRANSACTION WITH SHARED <engine> SNAPSHOT
START TRANSACTION WITH EXISITING <engine> SNAPSHOT X

CREATE EXPLICIT <engine> SNAPSHOT
ATTACH EXPLICIT <engine> SNAPSHOT X
RELEASE EXPLICIT <engine> SNAPSHOT

Supporting START TRANSACTION WITH CONSISTENT [ROCKSDB] SNAPSHOT
This adds two features in RocksDB.
1. Supporting START TRANSACTION WITH CONSISTENT SNAPSHOT
2. Getting current binlog position in addition to https://github.com/facebook/mysql-5.6/issues/1.
With these features, mysqldump can take consistent logical backup.

The 

sql/transaction.cc
@@ -122,7 +122,7 @@ bool trans_check_state(THD *thd) {
   @retval true   Failure
 */
 
-bool trans_begin(THD *thd, uint flags, bool *need_ok) {
+bool trans_begin(THD *thd, uint flags, bool *need_ok, handlerton *hton) {
   bool res = false;
   DBUG_TRACE;
 
@@ -188,25 +188,31 @@ bool trans_begin(THD *thd, uint flags, bool *need_ok) {
 
   if (tst) tst->add_trx_state(thd, TX_EXPLICIT);
 
+  snapshot_info_st ss_info;
+  ss_info.snapshot_id = thd->lex->snapshot_id;
+
   /* ha_start_consistent_snapshot() relies on OPTION_BEGIN flag set. */
   if (flags & MYSQL_START_TRANS_OPT_WITH_CONS_SNAPSHOT) {
     if (tst) tst->add_trx_state(thd, TX_WITH_SNAPSHOT);
-    res = ha_start_consistent_snapshot(thd, NULL, NULL, NULL, NULL, NULL);
-  } else if (flags & MYSQL_START_TRANS_OPT_WITH_CONS_INNODB_SNAPSHOT) {
-    char binlog_file[FN_REFLEN];
-    ulonglong binlog_pos;
-    char *gtid_executed = NULL;
-    int gtid_executed_length;
-    ulonglong snapshot_hlc = 0;
-    DBUG_ASSERT(need_ok != NULL);
-
-    res = (ha_start_consistent_snapshot(thd, binlog_file, &binlog_pos,
-                                        &gtid_executed, &gtid_executed_length,
-                                        &snapshot_hlc) ||
-           show_master_offset(thd, binlog_file, binlog_pos, gtid_executed,
-                              gtid_executed_length, snapshot_hlc, need_ok));
-
-    my_free(gtid_executed);
+    res = ha_start_consistent_snapshot(thd, nullptr, nullptr);
+  } else if (flags & MYSQL_START_TRANS_OPT_WITH_CONS_ENGINE_SNAPSHOT) {
+    DBUG_ASSERT(need_ok != nullptr);
+    /*
+      Even though a single engine is specified, for safety, all
+      engines should start a consistent snapshot.
+     */
+    res = ha_start_consistent_snapshot(thd, &ss_info, nullptr) ||
+          show_master_offset(thd, ss_info, need_ok);
+  } else if (flags & MYSQL_START_TRANS_OPT_WITH_SHAR_ENGINE_SNAPSHOT) {
+    DBUG_ASSERT(need_ok != nullptr);
+    ss_info.op = snapshot_operation::SNAPSHOT_CREATE;
+    res = ha_start_shared_snapshot(thd, &ss_info, hton) ||
+          show_master_offset(thd, ss_info, need_ok);
+  } else if (flags & MYSQL_START_TRANS_OPT_WITH_EXIS_ENGINE_SNAPSHOT) {
+    DBUG_ASSERT(need_ok != nullptr);
+    ss_info.op = snapshot_operation::SNAPSHOT_ATTACH;
+    res = ha_start_shared_snapshot(thd, &ss_info, hton) ||
+          show_master_offset(thd, ss_info, need_ok);
   }
 
   /*

ldonoso/percona-server

sql/transaction.cc
@@ -122,7 +122,7 @@ bool trans_check_state(THD *thd) {
   @retval true   Failure
 */
 
-bool trans_begin(THD *thd, uint flags, bool *need_ok) {
+bool trans_begin(THD *thd, uint flags, bool *need_ok, handlerton *hton) {
   bool res = false;
   DBUG_TRACE;
 
@@ -188,25 +188,31 @@ bool trans_begin(THD *thd, uint flags, bool *need_ok) {
 
   if (tst) tst->add_trx_state(thd, TX_EXPLICIT);
 
+  snapshot_info_st ss_info;
+  ss_info.snapshot_id = thd->lex->snapshot_id;
+
   /* ha_start_consistent_snapshot() relies on OPTION_BEGIN flag set. */
   if (flags & MYSQL_START_TRANS_OPT_WITH_CONS_SNAPSHOT) {
     if (tst) tst->add_trx_state(thd, TX_WITH_SNAPSHOT);
-    res = ha_start_consistent_snapshot(thd, NULL, NULL, NULL, NULL, NULL);
-  } else if (flags & MYSQL_START_TRANS_OPT_WITH_CONS_INNODB_SNAPSHOT) {
-    char binlog_file[FN_REFLEN];
-    ulonglong binlog_pos;
-    char *gtid_executed = NULL;
-    int gtid_executed_length;
-    ulonglong snapshot_hlc = 0;
+    res = ha_start_consistent_snapshot(thd, nullptr, nullptr);
+  } else if (flags & MYSQL_START_TRANS_OPT_WITH_CONS_ENGINE_SNAPSHOT) {
     assert(need_ok != nullptr);
-
-    res = (ha_start_consistent_snapshot(thd, binlog_file, &binlog_pos,
-                                        &gtid_executed, &gtid_executed_length,
-                                        &snapshot_hlc) ||
-           show_master_offset(thd, binlog_file, binlog_pos, gtid_executed,
-                              gtid_executed_length, snapshot_hlc, need_ok));
-
-    my_free(gtid_executed);
+    /*
+      Even though a single engine is specified, for safety, all
+      engines should start a consistent snapshot.
+     */
+    res = ha_start_consistent_snapshot(thd, &ss_info, nullptr) ||
+          show_master_offset(thd, ss_info, need_ok);
+  } else if (flags & MYSQL_START_TRANS_OPT_WITH_SHAR_ENGINE_SNAPSHOT) {
+    assert(need_ok != nullptr);
+    ss_info.op = snapshot_operation::SNAPSHOT_CREATE;
+    res = ha_start_shared_snapshot(thd, &ss_info, hton) ||
+          show_master_offset(thd, ss_info, need_ok);
+  } else if (flags & MYSQL_START_TRANS_OPT_WITH_EXIS_ENGINE_SNAPSHOT) {
+    assert(need_ok != nullptr);
+    ss_info.op = snapshot_operation::SNAPSHOT_ATTACH;
+    res = ha_start_shared_snapshot(thd, &ss_info, hton) ||
+          show_master_offset(thd, ss_info, need_ok);
   }
 
   /*