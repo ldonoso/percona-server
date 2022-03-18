facebook/mysql-5.6

Disconnect the connection instead of returning error on shutdown
Summary:
Port to 8.0

During server shutdown, transactions may still be committed after
semisync has been disabled. The server should check and see if
the thread has been requested to be killed during commit(), and
disconnect the connection if so.

Reviewed By: abhinav04sharma

Differential Revision: D22932654

fbshipit-source-id: c38c65b33dd

sql/mysqld.cc
@@ -2154,6 +2154,18 @@ static void close_connections(void) {
 
   (void)RUN_HOOK(server_state, after_server_shutdown, (NULL));
 
+  DBUG_EXECUTE_IF("commit_on_shutdown_testing", {
+    THD *thd = new THD();
+    thd->thread_stack = reinterpret_cast<char *>(&(thd));
+    thd->store_globals();
+
+    const char act[] = "now signal shutdown_started";
+    DBUG_ASSERT(!debug_sync_set_action(current_thd, STRING_WITH_LEN(act)));
+
+    thd->restore_globals();
+    delete thd;
+  };);
+
   /*
     All threads have now been aborted. Stop event scheduler thread
     after aborting all client connections, otherwise user may

ldonoso/percona-server

sql/mysqld.cc
@@ -2471,6 +2471,18 @@ static void close_connections(void) {
 
   (void)RUN_HOOK(server_state, after_server_shutdown, (nullptr));
 
+  DBUG_EXECUTE_IF("commit_on_shutdown_testing", {
+    THD *thd = new THD();
+    thd->thread_stack = reinterpret_cast<char *>(&(thd));
+    thd->store_globals();
+
+    const char act[] = "now signal shutdown_started";
+    assert(!debug_sync_set_action(current_thd, STRING_WITH_LEN(act)));
+
+    thd->restore_globals();
+    delete thd;
+  };);
+
   /*
     All threads have now been aborted. Stop event scheduler thread
     after aborting all client connections, otherwise user may