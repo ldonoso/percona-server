facebook/mysql-5.6

inplace add partition (#1145) (#1145)
Summary:
This change is to enable inplace add partition(range/list) support for myrocks. Major code change is to iterator all partitions and find which partition need to be created, call SE(myrocks) API to create that table with constructed partition name.

This doesn't solve DDL crash safe issue.

These code are prototypes and need be refined..

Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1145

Differential Revision: D23266554

Pulled By: luqun

fbshipit-source-id: a4a812ef2b1

storage/rocksdb/ha_rocksdb.cc
@@ -11731,6 +11731,7 @@ int ha_rocksdb::external_lock(THD *const thd, int lock_type) {
 
     if (tx) {
       tx->io_perf_end_and_record(&m_io_perf);
+      DBUG_ASSERT(tx->m_n_mysql_tables_in_use > 0);
       tx->m_n_mysql_tables_in_use--;
       if (tx->m_n_mysql_tables_in_use == 0 &&
           !my_core::thd_test_options(thd,

ldonoso/percona-server

storage/rocksdb/ha_rocksdb.cc
@@ -11827,6 +11827,7 @@ int ha_rocksdb::external_lock(THD *const thd, int lock_type) {
 
     if (tx) {
       tx->io_perf_end_and_record(&m_io_perf);
+      assert(tx->m_n_mysql_tables_in_use > 0);
       tx->m_n_mysql_tables_in_use--;
       if (tx->m_n_mysql_tables_in_use == 0 &&
           !my_core::thd_test_options(thd,